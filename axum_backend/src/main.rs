3f87e507ffc6223138bf95ce34abca8870262852
Austin Jones <ajones53.aj@gmail.com>
2025-06-08 20:16:11 -0400

    working

--- axum_backend/src/main.rs
+++ axum_backend/src/main.rs
@@ -8,7 +8,7 @@ use axum::{
     extract::{Path, State},
     http::StatusCode,
     response::{Html, IntoResponse},
-    routing::{get, post},
+    routing::{get, patch, post},
 };
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -22,6 +22,8 @@ async fn main() {
         .route("/", get(handler))
         .route("/games", get(get_games).post(add_game))
         .route("/games/{id}/accept", post(accept_game))
+        .route("/games/{id}/status", get(status_game))
+        .route("/games/{id}/play", patch(play_game))
         .with_state(db);
 
     // run it
@@ -38,11 +40,31 @@ async fn handler() -> Html<&'static str> {
 }
 
 #[derive(Default, Serialize, Clone)]
+enum GameState {
+    #[default]
+    Started,
+    Playing,
+    NaughtWin,
+    CrossWin,
+    Draw,
+}
+
+impl GameState {
+    fn is_terminal(&self) -> bool {
+        match self {
+            Self::NaughtWin | Self::CrossWin | Self::Draw => true,
+            _ => false,
+        }
+    }
+}
+
+#[derive(Default, Serialize, Clone)]
 struct Game {
     id: Uuid,
     player1_id: Uuid,
     player2_id: Option<Uuid>,
-    state: [u8; 16],
+    game_state: GameState,
+    player_moves: [Option<u8>; 16],
 }
 
 async fn get_games(State(db): State<Db>) -> impl IntoResponse {
@@ -72,7 +94,8 @@ async fn add_game(State(db): State<Db>, Json(input): Json<CreateGame>) -> impl I
         id: Uuid::new_v4(),
         player1_id: input.player1_id,
         player2_id: None,
-        state: [0u8; 16],
+        game_state: GameState::Started,
+        player_moves: [None; 16],
     };
 
     db.insert(game.id, game.clone());
@@ -96,7 +119,73 @@ async fn accept_game(
         return StatusCode::NOT_FOUND;
     };
 
+    game.game_state = GameState::Playing;
     game.player2_id = Some(input.player2_id);
+
+    StatusCode::ACCEPTED
+}
+
+async fn status_game(Path(game_id): Path<Uuid>, State(db): State<Db>) -> impl IntoResponse {
+    let db = db.lock().unwrap();
+
+    let Some(game) = db.get(&game_id) else {
+        return (StatusCode::NOT_FOUND, Json(None));
+    };
+
+    let status = match game.player2_id {
+        Some(_) => StatusCode::ACCEPTED,
+        None => StatusCode::CREATED,
+    };
+
+    (status, Json(Some(game.to_owned())))
+}
+
+#[derive(Default, Deserialize, Clone, Copy)]
+struct PlayGame {
+    player_id: Uuid,
+    player_move: u8,
+}
+
+async fn play_game(
+    Path(game_id): Path<Uuid>,
+    State(db): State<Db>,
+    Json(input): Json<PlayGame>,
+) -> impl IntoResponse {
+    let mut db = db.lock().unwrap();
+
+    let Some(game) = db.get_mut(&game_id) else {
+        return StatusCode::NOT_FOUND;
+    };
+
+    if game.player2_id.is_none() {
+        return StatusCode::FORBIDDEN;
+    }
+
+    let Some(next_move_index) = game.player_moves.iter().position(|m| m.is_none()) else {
+        return StatusCode::FORBIDDEN;
+    };
+
+    // even indexes go with player one
+    let one_or_two = next_move_index & 1 == 0;
+
+    // Check for the right player
+    if (one_or_two && input.player_id != game.player1_id)
+        || (!one_or_two && input.player_id != game.player2_id.unwrap())
+    {
+        return StatusCode::FORBIDDEN;
+    }
+
+    if game
+        .player_moves
+        .iter()
+        .filter(|m| m.is_some())
+        .map(|o| o.unwrap())
+        .any(|m| m == input.player_move)
+    {
+        return StatusCode::FORBIDDEN;
+    }
+
+    game.player_moves[next_move_index] = Some(input.player_move);
 
     StatusCode::ACCEPTED
 }
