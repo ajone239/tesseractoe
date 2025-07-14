aab8bea338fb627d9143b3a98b8a5b4e3ee80edc
Austin Jones <ajones53.aj@gmail.com>
2025-07-13 21:07:54 -0400

    start angular

---
+++ angular-toe/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `
+  <p> LFG! </p>
+  `,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-toe';
+}
