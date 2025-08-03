7cef0ac97f7fe37c7c9a74ab8a75e5a4151811d1
Austin Jones <ajones53.aj@gmail.com>
2025-08-03 12:38:19 -0400

    hungry

--- react-toe/src/app/page.tsx
+++ react-toe/src/app/page.tsx
@@ -1,4 +1,12 @@
+import { Game } from "@/types/game";
+import { useState } from "react";
+
 export default function Home() {
+    const [games, setGames] = useState<Game[]>([])
+
+    const openGames = games.filter(g => !g.player2_id);
+    const activeGames = games.filter(g => g.player2_id);
+
     return (
         <>
             <p>
